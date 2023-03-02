3efadabebcd4c36c86492a79d652ce4d5269fd5a
Abiudkimutai123 <gaming20kip@gmail.com>
2023-03-02 15:58:10 +0300

    first commit

---
+++ Loops/for_loops.py
@@ -0,0 +1,13 @@
+friends = ["mwangi", "wa iria","Jaba","Juma"]
+
+for i in friends:
+    print(i)
+
+numbers = ["1 2 3 4 5 6 7 8 9 10"]
+
+for i in numbers:
+    print(i)
+numbers = ["1","2","3","4","5","6",'7',"8","9","10"]
+
+for n in numbers:
+     print(n)
