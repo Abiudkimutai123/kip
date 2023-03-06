add909f3a70ac4ed6cf3127fcbd934e38fe482f4
Abiudkimutai123 <gaming20kip@gmail.com>
2023-03-06 15:53:05 +0300

    first commit

---
+++ OOP/House.py
@@ -0,0 +1,7 @@
+class House:
+    def __init__(self, window,color,bedroom,kitchen,verandah  ):
+        self.window = window
+        self.color = color
+        self.bedroom = bedroom
+        self.kitchen = kitchen
+        self.verandah = verandah
---
+++ OOP/House_2.py
@@ -0,0 +1,9 @@
+from House import House
+
+house_one = House("Frosted","White","Master bedroom","L-shaped","Gable")
+house_two = House("Awning","Grey","Guest bedroom","U-shaped","Sunroof")
+house_three = House("Single hang","Cyan","Teen`s bedroom","Island","Gazebo")
+
+print(house_one.window, house_one.color,house_one.bedroom,house_one.kitchen,house_one.verandah)
+print(house_two.window, house_two.color,house_two.bedroom,house_two.kitchen,house_two.verandah)
+print(house_three.window, house_three.color,house_three.bedroom,house_three.kitchen,house_three.verandah)
